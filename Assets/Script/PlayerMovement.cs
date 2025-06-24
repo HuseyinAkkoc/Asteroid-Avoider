4055bd1376c6cd2f1d6ba1e2f9cdc73554926146
HuseyinAkkoc <156607663+HuseyinAkkoc@users.noreply.github.com>
2025-06-24 17:37:45 -0400

    -screen to world point converting . -Control the tocuh screen.

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.Android;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -6,11 +8,39 @@ public class PlayerMovement : MonoBehaviour
 
     void Start()
     {
-      mainCamera = Camera.main; 
-    }
+      mainCamera = Camera.main;
+        if (mainCamera == null)
+        {
+            Debug.LogError(" Camera.main is NULL! Check camera setup.");
+        }
+        else
+        {
+            Debug.Log(" Camera found: " + mainCamera.name);
+        }
 
+    }
+ 
     void Update()
     {
-        
+        if (Touchscreen.current == null)
+        {
+            Debug.LogWarning(" Touchscreen input not available — are you running this on PC?");
+            return;
+        }
+
+        if (Touchscreen.current.primaryTouch.press.isPressed)
+        {
+            Vector2 touchPosition= Touchscreen.current.primaryTouch.position.ReadValue();
+
+            Debug.Log(" Screen position" + touchPosition);
+            
+            Vector3 ConvertPos= new Vector3 (touchPosition.x,touchPosition.y, 10f);
+            // screen to world is giving z default if not assign z
+            //To avaoid this , I am using another  (positive) Vctor3 varibale to convert with a fixed Z value.
+
+            Vector3 worldPosition= mainCamera.ScreenToWorldPoint(ConvertPos);
+
+            Debug.Log(" World position" + worldPosition);
+        }
     }
 }
