c37f592072dbf2dae9a92b0769d16d3b6d2ea477
HuseyinAkkoc <156607663+HuseyinAkkoc@users.noreply.github.com>
2025-06-24 16:56:33 -0400

    Add project files.

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Camera mainCamera;
+
+    void Start()
+    {
+      mainCamera = Camera.main; 
+    }
+
+    void Update()
+    {
+        
+    }
+}
